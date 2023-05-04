27a041c022598ecc63fb5b4c6135d5f61dffb490
Jakub Szymański <jakubszymanski@hotmail.com>
2023-05-04 02:31:43 +0200

    refactored company

--- src/app/model/templateElements/DetailElement.ts
+++ src/app/model/templateElements/DetailElement.ts
@@ -3,12 +3,14 @@ export class DetailElement {
   value: any;
   displayEditButton: boolean;
   name: string
+  picture?: boolean;
 
 
-  constructor(title: string, value: any, displayEditButton: boolean, name: string) {
+  constructor(title: string, value: any, displayEditButton: boolean, name: string, picture?: boolean) {
     this.title = title;
     this.value = value;
     this.displayEditButton = displayEditButton;
     this.name = name;
+    this.picture = picture
   }
 }
--- src/app/views/adminViews/company/company.component.ts
+++ src/app/views/adminViews/company/company.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {Company} from "../../../model/Company";
-import {Address} from "../../../model/Address";
 import {CompanyService} from "../../../service/company.service";
-import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {DomSanitizer} from '@angular/platform-browser';
 import {CompanyUpdateDto} from "../../../model/CompanyUpdateDto";
+import {DetailElement} from "../../../model/templateElements/DetailElement";
+import {UpdateFormElement} from "../../../model/templateElements/UpdateFormElement";
 
 @Component({
   selector: 'app-company',
@@ -11,159 +12,103 @@ import {CompanyUpdateDto} from "../../../model/CompanyUpdateDto";
   styleUrls: ['./company.component.scss']
 })
 export class CompanyComponent {
-  company: Company;
-  displayElementsCompany: string[][];
-  displayElementsAddress: string[][];
-  image: SafeUrl;
+  elements: DetailElement[];
+  updateElement: UpdateFormElement;
   modalVisible: boolean;
   modalHeader: string;
-  inputValue: any;
-  fieldNames: string[];
-  activeFieldName: string;
-
-  //TODO: temporal company id. In next version it would be provided from logged user info
-  companyId: number;
 
   constructor(private companyService: CompanyService,
               private sanitizer: DomSanitizer) {
-    this.companyId = 1;
   }
 
   ngOnInit() {
     this.modalVisible = false;
-    this.fieldNames = ['Name', 'Domain', 'Logotype', 'Zip code', 'Town', 'Street', 'House number']
-    this.companyService.findById(this.companyId)
-      .subscribe(data => {
-        this.company = data;
-        this.refreshDisplay(this.company);
-      })
+    this.loadElements();
   }
 
-  showEditModal(fieldName: string) {
-    this.modalHeader = 'edit: ' + fieldName;
+  showEditModal(element: DetailElement) {
+    this.modalHeader = 'edit: ' + element.title;
+    this.updateElement = new UpdateFormElement(element.value, element.name);
     this.modalVisible = true;
-    this.activeFieldName = fieldName;
-
-    switch (fieldName) {
-      case this.fieldNames[0]:
-        this.inputValue = this.company.name;
-        break;
-      case this.fieldNames[1]:
-        this.inputValue = this.company.domain;
-        break;
-      case this.fieldNames[2]:
-        this.inputValue = this.company.logotype;
-        break;
-      case this.fieldNames[3]:
-        this.inputValue = this.company.address.zipCode;
-        break;
-      case this.fieldNames[4]:
-        this.inputValue = this.company.address.town;
-        break;
-      case this.fieldNames[5]:
-        this.inputValue = this.company.address.street;
-        break;
-      case this.fieldNames[6]:
-        this.inputValue = this.company.address.houseNumber;
-        break;
-    }
-
   }
 
   onSubmit() {
-    let updateCompany: CompanyUpdateDto = this.createUpdateCompanyInstance()
-    this.companyService.update(this.companyId, updateCompany).subscribe(
-      data => {
-        this.company = data;
-        this.refreshDisplay(this.company);
-      }
-    );
+    let companyUpdateDto: CompanyUpdateDto = this.createUpdateDto();
+    this.updateCompanyAndRefreshDisplay(companyUpdateDto);
     this.modalVisible = false;
   }
 
-  createUpdateCompanyInstance(): CompanyUpdateDto {
-    switch (this.activeFieldName) {
-      case this.fieldNames[0]:
-        return {name: this.inputValue};
-      case this.fieldNames[1]:
-        return {domain: this.inputValue};
-      case this.fieldNames[2]:
-        return {logotype: this.inputValue};
 
-      case this.fieldNames[3]:
-        return {
-          address:
-            {
-              zipCode: this.inputValue,
-              town: this.company.address.town,
-              street: this.company.address.street,
-              houseNumber: this.company.address.houseNumber
-            }
-        };
-      case this.fieldNames[4]:
-        return {
-          address:
-            {
-              zipCode: this.company.address.zipCode,
-              town: this.inputValue,
-              street: this.company.address.street,
-              houseNumber: this.company.address.houseNumber
-            }
-        };
-      case this.fieldNames[5]:
-        return {
-          address:
-            {
-              zipCode: this.company.address.zipCode,
-              town: this.company.address.town,
-              street: this.inputValue,
-              houseNumber: this.company.address.houseNumber
-            }
-        };
-      case this.fieldNames[6]:
-        return {
-          address:
-            {
-              zipCode: this.company.address.zipCode,
-              town: this.company.address.town,
-              street: this.company.address.street,
-              houseNumber: this.inputValue
-            }
-        };
-      default:
-        throw new Error('internal error')
+  private loadElements() {
+    let id = this.getCompanyId();
+    this.getCompanyAndCreateElements(id);
+  }
+
+  //TODO: temporal company id. In next version it would be provided from logged user info
+  private getCompanyId() {
+    return 1;
+  }
+
+  private getCompanyAndCreateElements(id: number) {
+    this.companyService.findById(id).subscribe(data => {
+      this.createElements(data);
+    })
+  }
+
+  private createElements(company: Company) {
+    let pictureSafeUrl = this.getPictureSafeUrl(company.logotype)
+    this.elements = [
+      new DetailElement('Name', company.name, true, 'name'),
+      new DetailElement('Domain', company.domain, true, 'domain'),
+      new DetailElement('Logotype', pictureSafeUrl, false, 'logotype', true),
+      new DetailElement('Zip Code', company.address.zipCode, true, 'zipCode'),
+      new DetailElement('Town', company.address.town, true, 'town'),
+      new DetailElement('Street', company.address.street, true, 'street'),
+      new DetailElement('HouseNumber', company.address.houseNumber, true, 'houseNumber'),
+    ]
+  }
+
+  private getPictureSafeUrl(logotype: Int8Array) {
+    let objectURL = 'data:image/png;base64,' + logotype;
+    return this.sanitizer.bypassSecurityTrustUrl(objectURL);
+  }
+
+  private createUpdateDto() {
+    if (this.updateElement.name === 'zipCode' || this.updateElement.name === 'town' || this.updateElement.name === 'street' || this.updateElement.name === 'houseNumber') {
+      return this.createUpdateDtoWithAddress();
+    } else {
+      return this.createUpdateDtoWithBasicField();
     }
   }
 
-  private refreshDisplay(company: Company) {
-    this.refreshDisplayCompanyBaseFields(company);
-    this.refreshDisplayCompanyAddress(company.address != null ? company.address : this.createEmptyAddress())
-    this.refreshLogotype(company.logotype);
+  private createUpdateDtoWithBasicField() {
+    let o = Object.defineProperty({}, this.updateElement.name, {
+      value: this.updateElement.value,
+      writable: true,
+      enumerable: true,
+      configurable: true,
+    });
+    return (o as CompanyUpdateDto)
   }
 
-  private refreshDisplayCompanyBaseFields(company: Company) {
-    this.displayElementsCompany = [
-      [this.fieldNames[0], company.name],
-      [this.fieldNames[1], company.domain],
-    ]
+  private createUpdateDtoWithAddress() {
+    return {
+      address:
+        {
+          zipCode: this.updateElement.name === 'zipCode' ? this.updateElement.value : this.elements.find(e => e.name === 'zipCode')?.value,
+          town: this.updateElement.name === 'town' ? this.updateElement.value : this.elements.find(e => e.name === 'town')?.value,
+          street: this.updateElement.name === 'street' ? this.updateElement.value : this.elements.find(e => e.name === 'street')?.value,
+          houseNumber: this.updateElement.name === 'houseNumber' ? this.updateElement.value : this.elements.find(e => e.name === 'houseNumber')?.value,
+        }
+    };
   }
 
-  private refreshDisplayCompanyAddress(address: Address) {
-    this.displayElementsAddress = [
-      [this.fieldNames[3], address.zipCode],
-      [this.fieldNames[4], address.town],
-      [this.fieldNames[5], address.street],
-      [this.fieldNames[6], address.houseNumber]
-    ]
+  private updateCompanyAndRefreshDisplay(dto: CompanyUpdateDto) {
+    let id = this.getCompanyId();
+    this.companyService.update(id, dto).subscribe(
+      data => {
+        this.createElements(data);
+      }
+    );
   }
-
-  private createEmptyAddress(): Address {
-    return {zipCode: '', town: '', houseNumber: '', street: ''}
-  }
-
-  private refreshLogotype(logotype: Int8Array) {
-    let objectURL = 'data:image/png;base64,' + logotype;
-    this.image = this.sanitizer.bypassSecurityTrustUrl(objectURL);
-  }
-
 }
